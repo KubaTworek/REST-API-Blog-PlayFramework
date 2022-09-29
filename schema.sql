8c6fd73076987429e1acf58af58d321f988b6869
KubaTworek <kubatvorek@gmail.com>
2022-09-29 21:34:10 +0200

    add database schema

---
+++ schema.sql
@@ -0,0 +1,31 @@
+CREATE DATABASE IF NOT EXISTS `blog-db`;
+USE `blog-db`;
+
+CREATE TABLE IF NOT EXISTS `User` (
+                          `id` int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+                          `firstName` varchar(20) NOT NULL,
+                          `lastName` varchar(20) NOT NULL
+) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
+
+CREATE TABLE IF NOT EXISTS `Post` (
+                           `id` int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+                           `title` varchar(50) NOT NULL,
+                           `text` LONGTEXT NOT NULL,
+                           `user_id` int NOT NULL,
+                           FOREIGN KEY(`user_id`) REFERENCES `User`(`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
+
+CREATE TABLE IF NOT EXISTS `Comment` (
+                            `id` int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+                            `content` varchar(20) NOT NULL,
+                            `post_id` int NOT NULL,
+                            FOREIGN KEY(`post_id`) REFERENCES `Post`(`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
+
+CREATE TABLE IF NOT EXISTS `AuthorComment` (
+                           `id` int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+                           `user_id` int NOT NULL,
+                           `comment_id` int NOT NULL,
+                           FOREIGN KEY(`user_id`) REFERENCES `User`(`id`),
+                           FOREIGN KEY(`comment_id`) REFERENCES `Comment`(`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
